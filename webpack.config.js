4bb71cc3b8fed223fc35dd4d2dde973c5fdee3be
Barin Britva <barinbritva@outlook.com>
2023-11-06 00:56:53 +0400

    Edit deploy.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 //webpack.config.js
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  watch: true,
-  entry: {
-    index: "./src/index.ts",
-  },
-  output: {
-    path: path.resolve(__dirname, "./public/scripts"),
-    filename: "index.js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        options: { configFile: "tsconfig-dev.json" },
-      },
-    ],
-  },
+	mode: 'development',
+	devtool: 'inline-source-map',
+	watch: process.env.NODE_ENV !== 'production',
+	entry: {
+		index: './src/index.ts'
+	},
+	output: {
+		path: path.resolve(__dirname, './public/scripts'),
+		filename: 'index.js'
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+				options: {configFile: 'tsconfig-dev.json'}
+			}
+		]
+	}
 };
